1a5461e830bc224c0196c13b68a2e2d9672c051d
Markiyan <melnik.markiyan@gmail.com>
2022-07-02 21:05:44 +0300

    Made a raw version of UI

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
-let choice;
+// let choice;
+// function oneRound(playerSelection, computerSelection) {
+//     playerSelection = playerSelection.toLowerCase();
+//     computerSelection = computerSelection.toLowerCase();
+//     if (playerSelection ===  computerSelection) {
+//         return 'tie';
+//     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+//         return 'player';
+//     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+//         return 'player';
+//     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+//         return 'player';
+//     } else {
+//         return 'computer';
+//     }
+// }
 
-function oneRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    if (playerSelection ===  computerSelection) {
-        return `Tie! ${playerSelection} don\`t beat and don\`t lose to ${computerSelection}`;
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'Player win!';
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return 'Player win!';
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'Player win!';
-    } else {
-        return 'Player lost!';
-    }
-}
-
-function computerPlay() {
-    const choice = Math.floor(Math.random() * 3);
-    if (choice === 0){
-        return 'Paper';
-    } else if (choice === 1) {
-        return 'Rock';
-    } else {
-        return 'Scissors';
-    }
-}
+// function computerPlay() {
+//     const choice = Math.floor(Math.random() * 3);
+//     if (choice === 0){
+//         return 'Paper';
+//     } else if (choice === 1) {
+//         return 'Rock';
+//     } else {
+//         return 'Scissors';
+//     }
+// }
  
-function game() {
-    let computerSelection = computerPlay();
-    console.log('User sleection:', playerSelection, 'Computer selection:', computerSelection);
-    console.log(oneRound(computerSelection, playerSelection));
-    }
-}
-game();
+// function game() {
+//     let playerPoints = 0;
+//     let computerPoints = 0;
+//     let computerSelection = computerPlay();
+//     console.log('User sleection:', playerSelection, 'Computer selection:', computerSelection);
+//     winner = oneRound(computerSelection, playerSelection);
+//     if (winner === 'player') playerPoints += 1;
+//     else if (winner === 'computer') computerPoints += 1;
+// }
+// game();
